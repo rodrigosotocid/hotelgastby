c9b461045f7e69881698bb256da1b00a4b82ebae
Rodrigo <istikis@gmail.com>
2021-03-12 17:32:33 +0100

    Preview de post de habitaciones en la Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,51 @@
 import * as React from "react";
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import Layout from '../components/layout';
 import ImagenHotel from '../components/imagenHotel';
 import ContenidoInicio from '../components/contenidoInicio';
+import HabitacionPreview from '../components/habitacionPreview';
+import useHabitaciones from '../hooks/use-habitaciones';
 
-/* import { StaticImage } from "gatsby-plugin-image" */
+const ListadoHabitaciones = styled.ul`
+  max-width: 1200px;
+  width: 95%;
+  margin: 4rem auto 0 auto;
 
+  @media(min-width: 768px){
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 3rem;
+  }
+`;
 
-const IndexPage = () => (
+const IndexPage = () => {
 
-  <Layout>
-    <ImagenHotel />
-    <ContenidoInicio />
-  </Layout>
-)
+  const habitaciones = useHabitaciones();
+  console.log(habitaciones);
+  return (
+
+    <Layout>
+      <ImagenHotel />
+      <ContenidoInicio />
+
+      <h2
+        css={css`
+      text-align: center;
+      margin-top: 5rem;
+      font-size: 3rem;
+      `}
+      >Nuestras Habitaciones</h2>
+
+      <ListadoHabitaciones>{habitaciones.map(habitacion => (
+        <HabitacionPreview
+          key={habitacion.id}
+          habitacion={habitacion}
+        />
+      ))}</ListadoHabitaciones>
+
+    </Layout>
+  )
+}
 
 export default IndexPage
